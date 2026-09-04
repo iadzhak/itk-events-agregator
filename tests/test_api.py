4b9edca9f888359b08c0c09faa927c68cda49779
Iadzhak Sergei <yadzhak@yandex.ru>
2026-09-04 14:42:40 +0300

    api health

---
+++ tests/test_api.py
@@ -0,0 +1,11 @@
+import pytest
+from fastapi import status
+
+
+@pytest.mark.unit
+@pytest.mark.asyncio
+class TestHealth:
+    async def test_health_success(self, test_client):
+        response = await test_client.get('/health')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {'status': 'ok'}
