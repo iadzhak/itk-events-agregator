2aa846d79791d936b2ea2d104cd8fcd7b2a2fcf9
Iadzhak Sergei <yadzhak@yandex.ru>
2026-09-05 15:45:36 +0300

    add SyncMeta model

---
+++ src/app/core/base.py
@@ -0,0 +1,2 @@
+from app.core.db import Base  # noqa alembic
+from app.models.sync import SyncMeta  # noqa alembic
---
+++ src/app/models/sync.py
@@ -0,0 +1,22 @@
+import datetime as dt
+
+from sqlalchemy import Integer, String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.db import Base
+from app.types.sync_status import SyncStatus
+
+DEFAULT_LAST_CHANGED_AT = dt.datetime(2000, 1, 1, tzinfo=dt.timezone.utc)
+
+
+class SyncMeta(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    last_sync_time: Mapped[dt.datetime] = mapped_column(
+        DateTime,
+        default=lambda: dt.datetime.now(tz=dt.timezone.utc)
+    )
+    last_changed_at: Mapped[dt.datetime] = mapped_column(
+        DateTime,
+        default=DEFAULT_LAST_CHANGED_AT
+    )
+    sync_status: Mapped[SyncStatus] = mapped_column(String(10))
