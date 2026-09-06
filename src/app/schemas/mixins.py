8049b9d64f78c735f5611f1d2e76ecdf94f33cf7
Iadzhak Sergei <yadzhak@yandex.ru>
2026-09-06 13:54:55 +0300

    schemas divide CommonMixin

--- src/app/schemas/mixins.py
+++ src/app/schemas/mixins.py
@@ -4,8 +4,15 @@ from uuid import UUID
 from pydantic import BaseModel
 
 
-class CommonMixin(BaseModel):
+class BaseEntityMixin(BaseModel):
     id: UUID
     name: str
+
+
+class DateTrackMixin(BaseModel):
     changed_at: dt.datetime
     created_at: dt.datetime
+
+
+class CommonMixin(BaseEntityMixin, DateTrackMixin):
+    pass
