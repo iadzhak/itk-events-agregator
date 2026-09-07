fe904198fb9cb8a9c751d89a9f5a0156c8476dc4
Iadzhak Sergei <yadzhak@yandex.ru>
2026-09-07 16:30:33 +0300

    repo ticket

--- src/app/repository/__init__.py
+++ src/app/repository/__init__.py
@@ -2,10 +2,12 @@ __all__ = [
     'BaseRepository',
     'EventRepository',
     'PlaceRepository',
-    'SyncRepository'
+    'SyncRepository',
+    'TicketRepository'
 ]
 
 from app.repository.base import BaseRepository
 from app.repository.event import EventRepository
 from app.repository.place import PlaceRepository
 from app.repository.sync import SyncRepository
+from app.repository.ticket import TicketRepository
---
+++ src/app/repository/ticket.py
@@ -0,0 +1,5 @@
+from app.repository.base import BaseRepository
+
+
+class TicketRepository(BaseRepository):
+    pass
