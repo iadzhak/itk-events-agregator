97061b545427793e3d0b9a19a7cab707a9e84414
Iadzhak Sergei <yadzhak@yandex.ru>
2026-09-07 18:02:43 +0300

    add get_by_ticket_id method to TicketRepository

--- src/app/repository/ticket.py
+++ src/app/repository/ticket.py
@@ -1,5 +1,13 @@
+from uuid import UUID
+
+from sqlalchemy import select
+
+from app.models import Ticket
 from app.repository.base import BaseRepository
 
 
 class TicketRepository(BaseRepository):
-    pass
+    async def get_by_ticket_id(self, ticket_id: UUID) -> Ticket | None:
+        stmt = select(Ticket).where(Ticket.id == ticket_id)
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
