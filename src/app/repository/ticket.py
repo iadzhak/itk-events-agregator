c1c4a4eec0b14d6579efb6b8cc4677d23702f71b
Iadzhak Sergei <yadzhak@yandex.ru>
2026-09-07 18:51:10 +0300

    fix get_by_ticket_id TicketRepo

--- src/app/repository/ticket.py
+++ src/app/repository/ticket.py
@@ -8,7 +8,7 @@ from app.repository.base import BaseRepository
 
 class TicketRepository(BaseRepository):
     async def get_by_ticket_id(self, ticket_id: UUID) -> Ticket | None:
-        stmt = select(Ticket).where(Ticket.id == ticket_id)
+        stmt = select(Ticket).where(Ticket.ticket_id == ticket_id)
         result = await self.session.execute(stmt)
         return result.scalars().first()
 
