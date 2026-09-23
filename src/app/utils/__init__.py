2cd678ff92e2631fd2b5a30bae6e7fe6a7f37fe9
Iadzhak Sergei <yadzhak@yandex.ru>
2026-09-23 15:48:03 +0300

    implement hashing func

--- src/app/utils/__init__.py
+++ src/app/utils/__init__.py
@@ -2,6 +2,7 @@ __all__ = [
     'BasePaginatorFactory',
     'EventsPaginator',
     'get_events_paginator_class',
+    'make_payload_hash',
 ]
 
 from app.utils.events_paginator import (
@@ -9,3 +10,4 @@ from app.utils.events_paginator import (
     EventsPaginator,
     get_events_paginator_class,
 )
+from app.utils.make_payload_hash import make_payload_hash
