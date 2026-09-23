707c6b369d48ab7421033cd81c0c377be90b21c4
Iadzhak Sergei <yadzhak@yandex.ru>
2026-09-23 11:25:21 +0300

    implement EventRegistrationHandler

--- src/app/workers/handlers/event_registration.py
+++ src/app/workers/handlers/event_registration.py
@@ -8,6 +8,10 @@ class EventRegistrationHandler:
 
     async def handle(self, payload) -> None:
         try:
-            await self.client.notify(**payload)
+            await self.client.notify(
+                msg=payload.get('message'),
+                reference_id=payload.get('reference_id'),
+                idempotency_key=payload.get('idempotency_key'),
+            )
         except InternalApiError as e:
             raise HandlerError(str(e)) from e
